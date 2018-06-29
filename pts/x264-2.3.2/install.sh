8253e5724a0e93b137c473bf4e94f630885bcdac
Ross Burton <ross@burtonini.com>
2018-06-29 16:28:10 +0100

    Use 7za to unpack
    7z is the 'fully featured' plugin-based 7zip compressor, some systems only ship the self-contained 7za binary (but everywhere that has 7z has 7za) so use that.
    
    Also turn on set -e in the install script so that errors such as "7z: file not found" are not silently ignored.

--- pts/x264-2.3.2/install.sh
+++ pts/x264-2.3.2/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 mkdir x264_/
 
 tar -xjf nasm-2.13.01.tar.bz2
@@ -35,7 +37,7 @@ echo $? > ~/install-exit-status
 cd ~
 # rm -rf x264-snapshot-20180205-2245
 
-7z x Bosphorus_1920x1080_120fps_420_8bit_YUV_Y4M.7z
+7za x Bosphorus_1920x1080_120fps_420_8bit_YUV_Y4M.7z
 
 echo "#!/bin/sh
 ./x264_/bin/x264 -o /dev/null --slow --threads \$NUM_CPU_CORES Bosphorus_1920x1080_120fps_420_8bit_YUV.y4m > \$LOG_FILE 2>&1
